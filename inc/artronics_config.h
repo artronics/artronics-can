a9d1bd43fdd0f50c72aec6464bb99947608ec579
jalal <jalalhosseiny@gmail.com>
2021-01-15 23:02:35 +0000

    ART-07 add artronics_config.h file with can rx max buffer size

---
+++ inc/artronics_config.h
@@ -0,0 +1,11 @@
+#ifndef ARTRONICS_ARTRONICS_CONFIG_H
+#define ARTRONICS_ARTRONICS_CONFIG_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define ART_CAN_RX_BUF_SIZE 8
+
+#ifdef __cplusplus
+}
+#endif
+#endif //ARTRONICS_ARTRONICS_CONFIG_H
