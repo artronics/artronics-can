17e6655c1ed2962ce2a2352ceecaa3447b8473e2
jalal <jalalhosseiny@gmail.com>
2021-01-16 15:17:41 +0000

    ART-07 re-init buffer_ring before each test

--- tests/src/test_ring_buffer.cpp
+++ tests/src/test_ring_buffer.cpp
@@ -1,14 +1,17 @@
 #include "gtest/gtest.h"
 #include "ring_buffer.h"
 
-#define ARRAY_SIZE(arr)     (sizeof(arr) / sizeof((arr)[0]))
 #define BUFFER_SIZE 8
 
 class RingBufferTest : public ::testing::Test {
 protected:
     void SetUp() override {
-
+      // FIXME: the default value for RING_BUFFER_MAX must be big enough to contain all the buffers we're creating during tests
+      int err = RingBuffer_Init(&_rbd, &_attr);
+      ASSERT_EQ(0, err);
     }
+
+    rbd_t _rbd;
     char _buf[BUFFER_SIZE] = {0};
     rb_attr_t _attr = {
             .size_elem=sizeof(_buf[0]),
@@ -19,13 +22,11 @@ protected:
 
 TEST_F(RingBufferTest, RingBufer_Put_and_Get_ring_behaviour) {
   // Given
-  rbd_t rbd;
-  RingBuffer_Init(&rbd, &_attr);
+  char data = 'a';
 
   // When; put until full
-  char data = 'a';
   for (int i = 0; i < BUFFER_SIZE; ++i) {
-    RingBuffer_Put(rbd, &data);
+    RingBuffer_Put(_rbd, &data);
     data++;
   }
 
@@ -34,20 +35,20 @@ TEST_F(RingBufferTest, RingBufer_Put_and_Get_ring_behaviour) {
 
   char exp_data = 'a';
   for (int i = 0; i < BUFFER_SIZE; ++i) {
-    RingBuffer_Get(rbd, &act_data);
+    RingBuffer_Get(_rbd, &act_data);
     EXPECT_EQ(exp_data, act_data);
     exp_data++;
   }
 
   // When; put until full
   for (int i = 0; i < BUFFER_SIZE; ++i) {
-    RingBuffer_Put(rbd, &data);
+    RingBuffer_Put(_rbd, &data);
     data++;
   }
 
   // Then; consume until empty
   for (int i = 0; i < BUFFER_SIZE; ++i) {
-    RingBuffer_Get(rbd, &act_data);
+    RingBuffer_Get(_rbd, &act_data);
     EXPECT_EQ(exp_data, act_data);
     exp_data++;
   }
@@ -59,7 +60,7 @@ TEST_F(RingBufferTest, RingBufer_Init__buffer_size_should_be_power_of_two) {
   char buf[10] = {0};
   rb_attr_t attr = {
           .size_elem=sizeof(buf[0]),
-          .num_elem=ARRAY_SIZE(buf),
+          .num_elem=sizeof(buf)/sizeof(buf[0]),
           .buffer=buf,
   };
 
@@ -72,17 +73,17 @@ TEST_F(RingBufferTest, RingBufer_Init__buffer_size_should_be_power_of_two) {
 
 TEST_F(RingBufferTest, RingBufer_Put_fill_buffer) {
   // Given
-  rbd_t rbd;
-  RingBuffer_Init(&rbd, &_attr);
-
-  // When
   char data = 'a';
+
+  // fill buffer
   int no_err;
   int err;
   for (int i = 0; i < BUFFER_SIZE; ++i) {
-    no_err = RingBuffer_Put(rbd, &data);
+    no_err = RingBuffer_Put(_rbd, &data);
   }
-  err = RingBuffer_Put(rbd, &data);
+
+  // When
+  err = RingBuffer_Put(_rbd, &data);
 
   // Then
   ASSERT_EQ(0, no_err);
@@ -101,11 +102,13 @@ TEST_F(RingBufferTest, RingBufer_Put__struct) {
   test_data_t buf[2] = {0};
   rb_attr_t attr = {
           .size_elem=sizeof(buf[0]),
-          .num_elem=ARRAY_SIZE(buf),
+          .num_elem=sizeof(buf)/sizeof(buf[0]),
           .buffer=buf,
   };
 
   int err = RingBuffer_Init(&rbd, &attr);
+  ASSERT_EQ(0, err);
+
   test_data_t data0 = {.foo = 42, .bar=42, .baz = nullptr};
   test_data_t data1 = {.foo = 43, .bar=43, .baz = nullptr};
 
@@ -119,7 +122,6 @@ TEST_F(RingBufferTest, RingBufer_Put__struct) {
   RingBuffer_Get(rbd, &actData1);
 
   // Then
-  ASSERT_EQ(0, err);
 
   EXPECT_EQ(42, actData0.foo);
   EXPECT_EQ(42, actData0.bar);
