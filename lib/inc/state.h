4423244bd53538ca98bb8553d1b1fe12d9b52257
jalal <jalalhosseiny@gmail.com>
2021-01-20 21:38:03 +0000

    ART-16 add state module - empty impl

---
+++ lib/inc/state.h
@@ -0,0 +1,12 @@
+#ifndef ARTRONICS_STATE_H
+#define ARTRONICS_STATE_H
+#ifdef __cplusplus
+extern C {
+#endif
+
+int State_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif //ARTRONICS_STATE_H
---
+++ lib/state.c
@@ -0,0 +1,5 @@
+#include "state.h"
+
+int State_Init(void) {
+
+}
