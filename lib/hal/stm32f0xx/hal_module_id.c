5c05cdae5ade24c3e01f6380cefda77589920258
jalal <jalalhosseiny@gmail.com>
2021-02-26 23:02:11 +0000

    ART-13 add module_id implementation

--- lib/hal/stm32f0xx/hal_module_id.c
+++ lib/hal/stm32f0xx/hal_module_id.c
@@ -1,11 +1,27 @@
 #include "hal/hal_module_id.h"
+#include "stm32f0xx_hal.h"
 
-static const uint8_t module_id[8] = {0};
+static uint8_t module_id[8] = {0};
 
 int HalModuleId_init(void) {
+  uint32_t w = HAL_GetUIDw0();
+  module_id[0] = w;
+  module_id[1] = w >> 8;
+  module_id[2] = w >> 8 * 2;
+  module_id[3] = w >> 8 * 3;
 
+  w = HAL_GetUIDw1();
+  module_id[4] = w;
+  module_id[5] = w >> 8;
+  module_id[6] = w >> 8 * 2;
+  module_id[7] = w >> 8 * 3;
+
+  // TODO: the UID for stm32 is 96bit. We need a hash/digest, otherwise the uniqueness is not guaranteed.
+//  HAL_GetUIDw2();
+
+  return 0;
 }
 
-const uint8_t * HalModuleId_64b(void) {
+const uint8_t * HalModuleId_get64BitId(void) {
   return module_id;
 }
---
+++ tests/src/hal/stm32f0xx/test_module_id.cpp
@@ -0,0 +1,55 @@
+#include "gtest/gtest.h"
+#include "test_utils.h"
+#include "hal/hal_module_id.h"
+#include "stm32f0xx_hal.h"
+
+#include "fff.h"
+
+DEFINE_FFF_GLOBALS
+
+FAKE_VALUE_FUNC(uint32_t, HAL_GetUIDw0);
+FAKE_VALUE_FUNC(uint32_t, HAL_GetUIDw1);
+
+using namespace testing;
+using namespace test_utils::stub;
+
+class ModuleIdTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+      RESET_FAKE(HAL_GetUIDw0);
+      RESET_FAKE(HAL_GetUIDw1);
+      FFF_RESET_HISTORY();
+    }
+};
+
+TEST_F(ModuleIdTest, HalModuleId__should_get_UID_after_init) {
+  // When
+  auto status = HalModuleId_init();
+
+  // Then
+  ASSERT_EQ(status, 0);
+  ASSERT_EQ(HAL_GetUIDw0_fake.call_count, 1);
+  ASSERT_EQ(HAL_GetUIDw1_fake.call_count, 1);
+}
+
+TEST_F(ModuleIdTest, HalModuleId__should_get_moduleId_64bit) {
+  // Give
+  HAL_GetUIDw0_fake.return_val = 0x11223344;
+  HAL_GetUIDw1_fake.return_val = 0x55667788;
+
+  HalModuleId_init();
+
+  // When
+  auto *id = HalModuleId_get64BitId();
+
+  // Then
+  ASSERT_EQ(id[0], 0x44);
+  ASSERT_EQ(id[1], 0x33);
+  ASSERT_EQ(id[2], 0x22);
+  ASSERT_EQ(id[3], 0x11);
+
+  ASSERT_EQ(id[4], 0x88);
+  ASSERT_EQ(id[5], 0x77);
+  ASSERT_EQ(id[6], 0x66);
+  ASSERT_EQ(id[7], 0x55);
+}
